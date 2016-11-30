969845044bdd87584ea303e1c8e9097fd2354da3
AurelienNioche <nioche.aurelien@gmail.com>
2016-11-30 11:36:43 +0100

    various upgrades

--- task/stimuli_finder.py
+++ task/stimuli_finder.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 class StimuliFinder(object):
-
     def __init__(self):
+        self.possible_p = [0.2, 5, 0.5, 0.75, 1]
+        self.positive_x = np.arange(1, 5)
+        self.negative_x = np.arange(-4, 1)
 
         self.stimuli_parameters = {
             "left_p": 0,
@@ -17,118 +19,101 @@ class StimuliFinder(object):
 
         }
 
-        # ------------------------------- #
-
-        # # Uncomment for testing only probabilities
-        #
-        # self.possible_p = [0.25, 0.5, 0.75]
-        # self.possible_q = [1, 2, 3, 4]
-        #
-        # self.find_stimuli = {"fixed_q": (self.find_stimuli_fixed_q, None)}
-
-        # ------------------------------------- #
-
-        # Comment for testing only probabilities
-        self.possible_p = [0.25, 0.5, 0.75, 1]
-        self.possible_q = [1, 2, 3, 4]
-
-        self.expected_values = [-1.25, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.25]
-
-        # Associate a condition to a function to create stimuli parameters, with args
-
-        self.find_stimuli = dict()
-        self.find_stimuli["fixed_p"] = (self.find_stimuli_fixed_p, None)
-        self.find_stimuli["fixed_q"] = (self.find_stimuli_fixed_q, None)
-        self.find_stimuli["congruent"] = (self.find_stimuli_congruent, None)
-
-        for i in self.expected_values:
-            self.find_stimuli["incongruent_{}".format(i)] = (self.find_stimuli_incongruent, i)
-
-        # ------------------------------- #
-
-        self.possible_conditions = list(self.find_stimuli.keys())
-
-        # ------------------------------- #
-
         self.sides = ["left", "right"]
 
-        # ------------------------------- #
+        self.conditions = \
+            [
+                self.p_fixed_x0_change_possible_loss_x1_fixed_zero,
+                self.p_fixed_x0_change_positive_x1_fixed_possible_loss,
+                self.p_change_x0_fixed_positive_x1_fixed_possible_loss,
+                self.p_change_x0_fixed_possible_loss_x1_fixed_zero,
+                self.safe_vs_possible_loss,
+                self.random
+            ]
 
     def find(self):
+        np.random.choice(self.conditions, p=[0.1, 0.1, 0.1, 0.1, 0.3, 0.3])()
 
-        np.random.shuffle(self.sides)
-
-        condition = np.random.choice(self.possible_conditions)
-
-        # Call the good function according to the condition that has been selected
-        f, arg = self.find_stimuli[condition]
-        if arg is not None:
-            f(arg)
-        else:
-            f()
-
-        # Return stimuli parameters that have been created by function 'f'
         return self.stimuli_parameters
 
-    def find_stimuli_fixed_p(self):
+    def p_fixed_x0_change_possible_loss_x1_fixed_zero(self):
+        print("p: fixed; x0: possible loss; x1: equal to zero")
 
-        print("FIXED P")
+        single_p = np.random.choice(self.possible_p)
+        x0 = np.random.choice(list(self.positive_x) + list(self.negative_x), size=2, replace=False)
 
-        p_left = np.random.choice(self.possible_p)
-        x = np.random.choice(self.possible_q, size=2, replace=False)
+        self.assign_values(p=[single_p, single_p], x0=x0, x1=[0, 0])
 
-        self.assign_values([p_left, p_left], x)
+    def p_fixed_x0_change_positive_x1_fixed_possible_loss(self):
+        print("p: fixed; x0: possible loss; x1: equal to zero")
 
-    def find_stimuli_fixed_q(self):
+        single_p = np.random.choice(self.possible_p)
+        x0 = np.random.choice(self.positive_x, size=2, replace=False)
+        single_x1 = np.random.choice(list(self.negative_x) + [0])
 
-        print("FIXED Q")
+        self.assign_values(p=[single_p, single_p], x0=x0, x1=[single_x1, single_x1])
+
+    def p_change_x0_fixed_positive_x1_fixed_possible_loss(self):
+        print("p: change; x0: fixed and positive; x1: fixed and possible loss")
 
         p = np.random.choice(self.possible_p, size=2, replace=False)
-        x0 = np.random.choice(self.possible_q)
+        single_x0 = np.random.choice(self.positive_x)
+        single_x1 = np.random.choice(list(self.positive_x) + list(self.negative_x))
 
-        self.assign_values(p, [x0, x0])
+        self.assign_values(p=p, x0=[single_x0, single_x0], x1=[single_x1, single_x1])
 
-    def find_stimuli_congruent(self):
+    def p_change_x0_fixed_possible_loss_x1_fixed_zero(self):
+        print("p: change; x0: fixed and possible loss; x1: fixed and equal to zero")
 
-        print("CONGRUENT")
+        p = np.random.choice(self.possible_p, size=2, replace=False)
+        single_x0 = np.random.choice(list(self.positive_x) + list(self.negative_x))
 
-        p = np.sort(np.random.choice(self.possible_p, size=2, replace=False))
-        x = np.sort(np.random.choice(self.possible_q, size=2, replace=False))
+        self.assign_values(p=p, x0=[single_x0, single_x0], x1=[0, 0])
 
-        self.assign_values(p, x)
+    def safe_vs_possible_loss(self):
+        print("safe versus possible loss")
 
-    def find_stimuli_incongruent(self, expected_value):
+        p = sorted(np.random.choice(self.possible_p, size=2, replace=False), reverse=True)
+        x0 = sorted(np.random.choice(self.positive_x, size=2, replace=False))
+        x1 = [0, np.random.choice(self.negative_x)]
 
-        print("INCONGRUENT WITH DIFFERENCE IN EXPECTED VALUE OF {} IN (DE)FAVOR OF RISKY OPTION".format(expected_value))
+        self.assign_values(p=p, x0=x0, x1=x1)
 
-        p = np.sort(np.random.choice(self.possible_p, size=2, replace=False))
-        x = np.sort(np.random.choice(self.possible_q, size=2, replace=False))[::-1]
+    def random(self):
+        print("random")
 
-        while p[0]*x[0] - p[1]*x[1] != expected_value:
+        p = np.random.choice(self.possible_p, size=2, replace=False)
+        x0 = np.random.choice(self.positive_x, size=2, replace=False)
+        x1 = np.random.choice(list(self.negative_x) + [0], size=2, replace=False)
 
-            p = np.sort(np.random.choice(self.possible_p, size=2, replace=False))
-            x = np.sort(np.random.choice(self.possible_q, size=2, replace=False))[::-1]
+        self.assign_values(p=p, x0=x0, x1=x1)
 
-        self.assign_values(p, x)
-
-    def assign_values(self, p, x):
-
-        for i, side in enumerate(self.sides):
-
+    def assign_values(self, p, x0, x1):
+        idx = np.random.permutation(2)
+        for i, side in zip(idx, self.sides):
             self.stimuli_parameters["{}_p".format(side)] = p[i]
-            self.stimuli_parameters["{}_x0".format(side)] = x[i]
-            self.stimuli_parameters["{}_x1".format(side)] = 0
+            self.stimuli_parameters["{}_x0".format(side)] = x0[i]
+            self.stimuli_parameters["{}_x1".format(side)] = x1[i]
             self.stimuli_parameters["{}_beginning_angle".format(side)] = np.random.randint(0, 360)
 
 
 def main():
 
-    # Only for purpose of testing
-    stimuli_finder = StimuliFinder()
-    result = stimuli_finder.find()
-    print(result)
+    gain_expectation = []
+
+    for i in range(10000):
+        sf = StimuliFinder()
+        sp = sf.find()
+        gain_expectation.append(
+            np.mean(
+                [
+                    sp["left_p"] * sp["left_x0"] + (1 - sp["left_p"]) * sp["left_x1"],
+                    sp["right_p"] * sp["right_x0"] + (1 - sp["right_p"]) * sp["right_x1"]
+                ]
+            )
+        )
+    print("Gain expectation in average:", np.mean(gain_expectation))
 
 
 if __name__ == "__main__":
-
     main()
