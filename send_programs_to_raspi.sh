ecba6cd89c5078fcd7d929cf2b75b3c9176e2581
AurelienNioche <nioche.aurelien@gmail.com>
2017-03-08 14:39:37 +0100

    various upgrades

--- send_programs_to_raspi.sh
+++ send_programs_to_raspi.sh
@@ -1,6 +1,7 @@
-scp raspi/grip_manager.py pi@169.254.162.142:/home/pi/grip_manager.py
-scp raspi/pump_manager.py pi@169.254.162.142:/home/pi/pump_manager.py
-scp raspi/pump_manager.service pi@169.254.162.142:/home/pi/pump_manager.service
-scp raspi/grip_manager.service pi@169.254.162.142:/home/pi/grip_manager.service
-scp raspi/create_services.sh pi@169.254.162.142:/home/pi/create_services.sh
-scp raspi/test_services.sh pi@169.254.162.142:/home/pi/test_services.sh
+rp_ip_address=10.24.12.48
+scp raspi/grip_manager.py pi@${rp_ip_address}:/home/pi/grip_manager.py
+scp raspi/pump_manager.py pi@${rp_ip_address}:/home/pi/pump_manager.py
+scp raspi/pump_manager.service pi@${rp_ip_address}:/home/pi/pump_manager.service
+scp raspi/grip_manager.service pi@${rp_ip_address}:/home/pi/grip_manager.service
+scp raspi/create_services.sh pi@${rp_ip_address}:/home/pi/create_services.sh
+scp raspi/test_services.sh pi@${rp_ip_address}:/home/pi/test_services.sh
